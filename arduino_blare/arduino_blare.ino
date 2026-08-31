d61dc2e77967a06f443c76987cf1f8d51c5e842c
Tyo1epic <jhua38275@gmail.com>
2026-08-31 11:24:12 +0800

    Refactor UI text rendering with scroll support
    Introduce dynamic text positioning and horizontal scrolling for the display. Text now uses a map-based system for management and renders at regular intervals. The updatetime() function now calculates proper text bounds for centered display. New settext() and reset() utility functions improve code organization and reduce duplication.

--- arduino_blare/arduino_blare.ino
+++ arduino_blare/arduino_blare.ino
@@ -1,6 +1,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
+#include <map>
+#include <vector>
 
 #define TFT_SCLK 10 // labeled SCL on the screen
 #define TFT_MOSI 11 // labeled SDA on the screen
@@ -16,13 +18,24 @@
 
 #define buzzer 8
 
+struct TextItem {
+int x;
+int y;
+int w;
+};
+std::map<String, TextItem> Words;
 unsigned long temp = 0;
+unsigned long texttemp = 0;
 unsigned int sec = 0;
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 bool start = false;
 int text_size = 3;
 int time = 0;
 bool show = false;
+int offsety = 0;
+String timetext = "";
+TextItem timevalues;
+bool showtime = false;
 
 void setup() {
   Serial.begin(115200);
@@ -50,39 +63,34 @@ void setup() {
 
 void loop() {
   if ((digitalRead(first_) == LOW || digitalRead(second_) == LOW || digitalRead(third_) == LOW || digitalRead(fourth_) == LOW) && !start){
+    reset();
     tft.fillScreen(ST77XX_BLACK); 
-  tft.setCursor(0,0);
-    tft.println("please choose text size");
-    tft.print("1 = increase, 2 = decrease, 3 = enter, 4 = reset");
+    tft.setCursor(0,0);
+    settext("please choose text size");
+    settext("1 = increase, 2 = decrease, 3 = enter, 4 = reset");
+    updatetext();
     while(true){
       if(digitalRead(first_) == LOW){
         if (text_size < 10) text_size++;
-        clearboard();
-    tft.println("please choose text size");
-    tft.print("1 = increase, 2 = decrease, 3 = enter, 4 = reset");
+        updatetext();
     }else if(digitalRead(second_) == LOW){
       if (text_size > 1) text_size--;
-    clearboard();
-    tft.println("please choose text size");
-    tft.print("1 = increase, 2 = decrease, 3 = enter, 4 = reset");
+      updatetext();
     }else if(digitalRead(third_) == LOW){
       break;
     }else if(digitalRead(fourth_) == LOW){
       text_size = 3;
-    clearboard();
-    tft.println("please choose text size");
-    tft.print("1 = increase, 2 = decrease, 3 = enter, 4 = reset");
+      updatetext();
     }
     while(digitalRead(first_) == LOW || digitalRead(second_) == LOW || digitalRead(third_) == LOW || digitalRead(fourth_) == LOW){
       
-  tone(buzzer, 1000); // Play 1000 Hz tone
-  delay(100);           // Wait for 1 second
-  noTone(buzzer);     // Stop the sound
+  tone(buzzer, 1000, 100); // Play 1000 Hz tone
     }
     }
-    clearboard();
-    tft.println("please select current time");
-    tft.print("press any button to continue");
+    reset();
+    settext("please select current time");
+    settext("press any button to continue");
+    updatetext();
     while(digitalRead(first_) && digitalRead(second_) && digitalRead(third_) && digitalRead(fourth_)){delay(1); }
     updatetime();
   while(true){
@@ -100,13 +108,12 @@ void loop() {
     }
     while(digitalRead(first_) == LOW || digitalRead(second_) == LOW || digitalRead(third_) == LOW || digitalRead(fourth_) == LOW){
       
-  tone(buzzer, 1000); // Play 1000 Hz tone
-  delay(100);           // Wait for 1 second
-  noTone(buzzer);     // Stop the sound
+  tone(buzzer, 1000, 100); // Play 1000 Hz tone
     }
     }
     temp = millis();
     start = true;
+    showtime = true;
   }
   if (start){
 if (millis() - temp >= 1000) {
@@ -118,22 +125,75 @@ time++;}
 updatetime();
 }
   }
+if (millis() - texttemp >= 20) {
+  texttemp = millis();
+  if(showtime) showtimetext();else updatetext();
+}
+}
+void updatetext(){
+  clearboard();
+  for (auto& element : Words) {
+    String text = element.first;
+    TextItem& values = element.second;
+  if (values.w + values.x > tft.width()){
+    values.x--;
+  }else{
+    values.x = 0;
+  }
+    tft.setCursor(values.x, values.y);
+    tft.print(text);
+
+
+  }
+}
+void showtimetext(){
+  clearboard();
+    String text = timetext;
+    
+  if (timevalues.w + timevalues.x > tft.width()){
+    timevalues.x--;
+  }else{
+    timevalues.x = 0;
+  }
+    tft.setCursor(timevalues.x, timevalues.y);
+    tft.print(text);
 }
 void updatetime(){
-  clearboard();
   while(time < 0) time += 1440;
   while(time >= 1440) time -= 1440;
-  if(time / 60 < 10) tft.print("0");
-  tft.print(time / 60);
-  if(show)tft.print(":");else tft.print(" ");
-  if ((time % 60) < 10)
-  tft.print("0");
-  tft.print(time % 60);
+
+  String temp = "";
+  if(time / 60 < 10) temp += "0";
+  temp += String(time / 60);
+  if(show)temp += ":";else temp += " ";
+  if ((time % 60) < 10) temp += "0";
+  temp += String(time % 60);
+
+  timetext = temp;
+  int16_t x1, y1;
+  uint16_t w, h; 
+  tft.getTextBounds(timetext, 0, 0, &x1, &y1, &w, &h);
+  timevalues = {(tft.width() - w) / 2, (tft.height() - h) / 2, (int)w};
   
 }
+
+void settext(String text, int margin = 0, bool centre = false){
+  int16_t x1, y1;
+  uint16_t w, h; 
+  tft.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+  Words[text] = {centre ? ((tft.width() - w) / 2) : 0, offsety + margin, w};
+  offsety += h + margin;
+}
+
 void clearboard(){
   tft.setTextSize(text_size);
   tft.fillScreen(ST77XX_BLACK); 
   tft.setCursor(0,0);
+
+}
+void reset(){
+  clearboard();
+  offsety = 0;
+  Words.clear();
 
 }
